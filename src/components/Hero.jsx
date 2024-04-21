506512c239ec579b9fcda223e7d9952f08caf7e2
Majek11 <majeklekan9@gmail.com>
2024-04-21 01:25:40 +0100

    first-commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { logo } from '../assets'
+
+const Hero = () => {
+    return (
+        <header className='w-full flex justify-center items-center flex-col'>
+            <nav className='flex justify-between items-center w-full pt-3'>
+                <img src={logo} alt="logo" className='w-32 object-contain' />
+
+
+                <button type='button' onClick={() => window.open('https://github.com/majek11')}
+                    className='black_btn'>
+                    Github
+                </button>
+
+            </nav>
+
+            <h1 className='head_text font-["Poppins"]'>
+                Summarize Articles with <br className='max-md:hidden' />
+                <span className='purple_gradient '>OpenAI GPT-4</span>
+            </h1>
+            <h2 className='desc font-["Anek Odia"]'>
+                Streamline your reading experience with summaize, an open-source tool that condenses lengthy articles into concise, clear summaries.
+            </h2>
+
+
+        </header>
+    )
+}
+
+export default Hero
